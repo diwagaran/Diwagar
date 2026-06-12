c70dff77d5743f17b015daf8839d849ec2d9a09f
diwagaran <diwagardiwagar424@gmail.com>
2026-06-12 10:34:35 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+//using variable decleration with var
+var name="diwa"
+console.log(name)
+
+//using variable decleration with let
+let number="43"
+console.log(number)
+
+//using variable decleration with const
+const country="India"
+console.log(country)
